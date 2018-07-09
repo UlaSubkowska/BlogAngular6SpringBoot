815eda1975f87f2ae3053c28c8d478c4f89b7dab
Ula <urszulasubkowska@gmail.com>
2018-07-09 15:14:00 +0200

    Entities orm (#6)
    * new dependency, db connection config
    
    * jpa annotations added to entities classes

--- backend/src/main/java/blog/entites/Post.java
+++ backend/src/main/java/blog/entites/Post.java
@@ -2,14 +2,21 @@ package blog.entites;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @AllArgsConstructor
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 
 public class Post {
     private int userId;
+    @Id
     private int id;
     private String title;
     private String body;
